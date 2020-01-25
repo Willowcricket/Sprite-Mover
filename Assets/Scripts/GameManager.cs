591444a622d1b0b01041dd0b253313e05ec187cb
Willowcricket <willowcrickit@gmail.com>
2020-01-25 16:50:12 -0700

    Finishing touches

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject star; //This references the starShip game obj
+    public SpriteMover sm; //This references the Sprite Movement Component
+    public bool isActive = true; //Indicates whether the starShip GameObject is active.
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isActive = star.activeSelf; // The bool value is equal to the initial value of the object's active status
+        sm = star.GetComponent<SpriteMover>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            Application.Quit(); //Out of Engine
+        }
+        /* When the player presses Q
+         * We check to see if the object is active
+         * by checking the value of the boolean
+         * that we are using to identify
+         * the active state of the star ship.
+         */ 
+        if (Input.GetKeyDown(KeyCode.Q) && isActive)
+        {
+            star.SetActive(false);
+            isActive = false;
+        }
+        else if (Input.GetKeyDown(KeyCode.Q) && !isActive)
+        {
+            star.SetActive(true);
+            isActive = true;
+        }
+        if (Input.GetKeyDown(KeyCode.P)) //On the down press of an input, Do{};
+        {
+            sm.enabled = !sm.enabled; //Toggle the enabled property of the component.
+        }
+    }
+}
--- Assets/Scripts/SpriteMover.cs
+++ Assets/Scripts/SpriteMover.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class SpriteMover : MonoBehaviour
 {
     public Transform tf;
-    public float speed = 1.0f;
+    public float speed = 1.0f; //Editable in the Inspector
 
     // Start is called before the first frame update
     void Start()
@@ -29,6 +29,52 @@ public class SpriteMover : MonoBehaviour
             {
                 tf.position = tf.position + Vector3.up * Time.deltaTime * speed;
             }
+        }
+        if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
+        { //Holding Shift
+            if (Input.GetKeyDown(KeyCode.DownArrow))
+            {
+                tf.position = tf.position + Vector3.down * Time.deltaTime * speed;
+            }
+        }
+        else
+        { //Not holding Shift
+            if (Input.GetKey(KeyCode.DownArrow))
+            {
+                tf.position = tf.position + Vector3.down * Time.deltaTime * speed;
+            }
+        }
+        if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
+        { //Holding Shift
+            if (Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                tf.position = tf.position + Vector3.left * Time.deltaTime * speed;
+            }
+        }
+        else
+        { //Not holding Shift
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                tf.position = tf.position + Vector3.left * Time.deltaTime * speed;
+            }
+        }
+        if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
+        { //Holding Shift
+            if (Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                tf.position = tf.position + Vector3.right * Time.deltaTime * speed;
+            }
+        }
+        else
+        { //Not holding Shift
+            if (Input.GetKey(KeyCode.RightArrow))
+            {
+                tf.position = tf.position + Vector3.right * Time.deltaTime * speed;
+            }
+        }
+        if (Input.GetKey(KeyCode.Space))
+        {
+            tf.position = Vector3.zero; //new Vector3(0, 0, 0);
         }
     }
 }
